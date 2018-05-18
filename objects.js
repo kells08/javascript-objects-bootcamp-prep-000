30437b474ef8af4453359e750e3eb7d512df6723
Kelly Lwakatare <devkells00@gmail.com>
2018-05-18 04:32:12 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,5 +7,5 @@ function updatePlaylist (playlist, artistName, songTitle) {
 
 function removeFromPlaylist (playlist, artistName) {
  delete removeFromPlaylist.artistName;
- removeFromPlaylist;
+ return removeFromPlaylist;
 }
